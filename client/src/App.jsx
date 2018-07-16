af940f1db52bd1aa82b9564d2ca960f7c9b98f22
Aaron Wilder <element1028@gmaill.com>
2018-07-16 15:24:17 -0700

    dependencies updated/ style reset
    [x] add testing frameworks
    [x] add stripe package
    [x] add router
    [x] add maps component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Back Woods App</h1>
-      </div>
+      <React.Fragment>
+        <CssBaseline>
+          <h1>Back Woods App</h1>
+        </CssBaseline>
+      </React.Fragment>
     );
   }
 }
