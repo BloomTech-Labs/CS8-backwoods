94edc8fb3e71bafb7462cecfafe6d7499c708759
frogr <austindanielfrench@gmail.com>
2018-07-17 10:16:24 -0700

    basic structure for landing page (with debug still visible)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 // CssBaseline is the Material UI built in CSS reset
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoggedIn: false
+    };
+    this.handleLogIn = this.handleLogIn.bind(this);
+  }
+
+  handleLogIn(e) {
+    this.setState({ isLoggedIn: true });
+  }
+
   render() {
     return (
-      <React.Fragment>
-        <CssBaseline>
-          <h1>Back Woods App</h1>
-        </CssBaseline>
-      </React.Fragment>
+      <div>
+        <React.Fragment>
+          <CssBaseline>
+            <h1>Back Woods App</h1>
+            {/* DEBUG */}
+            <h3> Logged In: {this.state.isLoggedIn.toString()}</h3>
+            <Button variant="contained" color="primary">
+              Buy Now
+            </Button>
+          </CssBaseline>
+        </React.Fragment>
+      </div>
     );
   }
 }
