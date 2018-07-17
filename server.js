b5f801da59c7a0903d5c44aca85efecb47f12a33
Nick Coultas <nncoultas@yahoo.com>
2018-07-17 10:19:33 -0700

    model  set-up working on tests/connecting server to  database

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const app = require("express")();
+const app = require('express')();
 const bodyParser = require('body-parser');
-const cors = require("cors");
+const cors = require('cors');
 
-
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
+import models from './models';
 
 // Test GET request
 app.get('/', (req, res) => {
-  res.send('GET request')
-  })
+  res.send('GET request');
+});
 
-app.listen('8000',() => console.log("Server listening on port 8000"))
+models.sequelize.sync().then(() => {
+  app.listen('8000', () => console.log('Server listening on port 8000'));
+});
 
 module.exports = app;
