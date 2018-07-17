1b0a3e7bdfd084782b2d7db6fc9b98ff29d9daf4
Nick Coultas <nncoultas@yahoo.com>
2018-07-17 09:54:42 -0700

    setup user model and instaled bcrypt

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const bcrypt = require('bcrypt');
+
+const user = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'user',
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {
+      instanceMethods: {
+        generateHash(password) {
+          return bcrypt.hash(password, bcrypt.genSalt(8));
+        },
+        validPassword(password) {
+          return bcrypt.compare(password, this.password);
+        }
+      }
+    }
+  );
+  return User;
+};
+
+export default user;
