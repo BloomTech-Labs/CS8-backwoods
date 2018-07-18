7384997815858259d0f30f21bf688603210eff67
frogr <austindanielfrench@gmail.com>
2018-07-18 10:09:02 -0700

    carousel for the front end landing page

---
+++ client/src/components/Landing/Carousel.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+
+const tutorialSteps = [
+  {
+    label: '',
+    imgPath: 'https://imgur.com/UH0ctZa.jpg'
+  },
+  {
+    label: '',
+    imgPath: 'https://imgur.com/Uk6LitO.jpg'
+  },
+  {
+    label: '',
+    imgPath: 'https://imgur.com/tWejLD5.jpg'
+  }
+];
+
+const styles = theme => ({
+  root: {
+    maxWidth: 750,
+    flexGrow: 1,
+    margin: '0 auto'
+  },
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    height: 50,
+    paddingLeft: theme.spacing.unit * 4,
+    marginBottom: 20,
+    backgroundColor: theme.palette.background.default
+  },
+  img: {
+    height: 300,
+    maxWidth: 600,
+    overflow: 'hidden',
+    width: '100%'
+  }
+});
+
+class TextMobileStepper extends React.Component {
+  state = {
+    activeStep: 0
+  };
+
+  handleNext = () => {
+    this.setState(prevState => ({
+      activeStep: prevState.activeStep + 1
+    }));
+  };
+
+  handleBack = () => {
+    this.setState(prevState => ({
+      activeStep: prevState.activeStep - 1
+    }));
+  };
+
+  render() {
+    const { classes, theme } = this.props;
+    const { activeStep } = this.state;
+
+    const maxSteps = tutorialSteps.length;
+
+    return (
+      <div className={classes.root}>
+        <Paper square elevation={0} className={classes.header}>
+          <Typography>{tutorialSteps[activeStep].label}</Typography>
+        </Paper>
+        <img
+          className={classes.img}
+          src={tutorialSteps[activeStep].imgPath}
+          alt={tutorialSteps[activeStep].label}
+        />
+        <MobileStepper
+          steps={maxSteps}
+          position="static"
+          activeStep={activeStep}
+          className={classes.mobileStepper}
+          nextButton={
+            <Button
+              size="small"
+              onClick={this.handleNext}
+              disabled={activeStep === maxSteps - 1}
+            >
+              {theme.direction === 'rtl' ? (
+                <KeyboardArrowLeft />
+              ) : (
+                <KeyboardArrowRight />
+              )}
+            </Button>
+          }
+          backButton={
+            <Button
+              size="small"
+              onClick={this.handleBack}
+              disabled={activeStep === 0}
+            >
+              {theme.direction === 'rtl' ? (
+                <KeyboardArrowRight />
+              ) : (
+                <KeyboardArrowLeft />
+              )}
+            </Button>
+          }
+        />
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(TextMobileStepper);
