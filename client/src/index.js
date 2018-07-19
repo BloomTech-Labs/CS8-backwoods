4968a5e59d2e19917a7ea8f641b63f4e28237d90
frogr <austindanielfrench@gmail.com>
2018-07-19 12:54:45 -0700

    small file structure changes to avoid confusion and added routing

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import App from './App';
+import App as Landing from './components/Landing/App.jsx';
+import AccountForm from './components/Account/AccountForm.jsx';
+import BillingForm from './components/Billing/BillingForm.jsx';
+import DebugRoutes from './components/Debug/DebugRoutes.jsx';
+import Nav from './components/Nav/Nav.jsx';
+import Trip from './components/Trip/Trip.jsx';
+import TripCreate from './components/Trip/TripCreate.jsx';
+import TripList from './components/TripList/TripList.jsx';
+
+
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
@@ -13,9 +22,20 @@ if (module.hot) {
 ////////////////////////
 
 ReactDOM.render(
+  <div>
   <Router>
-    <App />
-  </Router>,
+    <Route path="/*" component={DebugRoutes} />
+
+    <Route exact path="/" component={Landing} />
+
+    <Route path="/trips/*" component={Nav} />
+    <Route exact path="/trips" component={TripList} />
+    <Route exact path="/trips/:id" component={Trip} />
+    <Route exact path="/trips/create" component={TripCreate} />
+    <Route exact path="/account/settings" component={AccountForm} />
+    <Route exact path="/account/billing" component={BillingForm} />
+  </Router>
+  </div>,
   document.getElementById('root')
 );
 registerServiceWorker();
