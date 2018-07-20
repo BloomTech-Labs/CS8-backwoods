d9cea5c38e7d794de911cf6cde60945be2c8d34b
Nick Coultas <nncoultas@yahoo.com>
2018-07-19 18:44:05 -0700

    Snackbar colors/icons Work in Progress

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,22 +4,98 @@ import axios from 'axios';
 import BuyNow from './components/Landing/BuyNow.jsx';
 import Modal from './components/Landing/Modal.jsx';
 import PageContent from './components/Landing/PageContent.jsx';
+
+
 import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import green from '@material-ui/core/colors/green';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import IconButton from '@material-ui/core/IconButton';
+import ErrorIcon from '@material-ui/icons/Error';
+import CloseIcon from '@material-ui/icons/Close';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import { withStyles } from '@material-ui/core/styles';
+
+const variantIcon = {
+  success: CheckCircleIcon,
+  error: ErrorIcon,
+};
+
+const MySnackbarContent = (props) => {
+  const { classes, className, message, onClose, variant, ...other } = props;
+  const Icon = variantIcon[variant];
+
+  return (
+    <SnackbarContent
+      className={classNames(classes[variant], className)}
+      aria-describedby="client-snackbar"
+      message={
+        <span id="client-snackbar" className={classes.message}>
+          <Icon className={classNames(classes.icon, classes.iconVariant)} />
+          {message}
+        </span>
+      }
+      action={[
+        <IconButton
+          key="close"
+          aria-label="Close"
+          color="inherit"
+          className={classes.close}
+          onClick={onClose}
+        >
+          <CloseIcon className={classes.icon} />
+        </IconButton>,
+      ]}
+      {...other}
+    />
+  );
+}
+
+MySnackbarContent.propTypes = {
+  classes: PropTypes.object.isRequired,
+  className: PropTypes.string,
+  message: PropTypes.node,
+  onClose: PropTypes.func,
+  variant: PropTypes.oneOf(['success', 'error']).isRequired,
+};
+
+const styles1 = theme => ({
+  success: {
+    backgroundColor: green[600],
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark,
+  },
+  icon: {
+    fontSize: 20,
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing.unit,
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
+
+const MySnackbarContentWrapper = withStyles(styles1)(MySnackbarContent);
 
 // CssBaseline is the Material UI built in CSS reset
 class App extends Component {
   constructor() {
     super();
     this.state = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
-        validatePassword: '',
-        isLoggedIn: false,
-        snackbarOpen: false,
-        snackbarVertical: 'top',
-        snackbarHorizontal: 'center'
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      validatePassword: '',
+      isLoggedIn: false,
+      snackbarOpen: false,
+      snackbarVertical: 'top',
+      snackbarHorizontal: 'center'
     };
     this.handleLogIn = this.handleLogIn.bind(this);
   }
@@ -40,13 +116,14 @@ class App extends Component {
 
   handleSignUp = e => {
     e.preventDefault();
-    const { firstName, lastName, email, password} = this.state;
+    const { firstName, lastName, email, password } = this.state;
     axios.post('http://localhost:8000/signup', { firstName, lastName, email, password })
       .then(res => {
         console.log(res)
         console.log(res.data)
       })
   }
+
   handleChange = name => event => {
     this.setState({
       [name]: event.target.value,
@@ -56,7 +133,10 @@ class App extends Component {
   handleLogIn(e) {
     this.setState({ isLoggedIn: true });
   }
-  handleSnackbarClose = () => {
+  handleSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
     this.setState({ snackbarOpen: false });
   };
 
@@ -64,26 +144,36 @@ class App extends Component {
     this.setState({ snackbarOpen: true, ...state });
   };
 
+
+
   render() {
     return (
       <div>
         <React.Fragment>
-        <Snackbar
-          anchorOrigin={{ vertical: this.state.snackbarVertical, horizontal: this.state.snackbarHorizontal }}
-          open={this.state.snackbarOpen}
-          onClose={this.handleSnackbarClose}
-          ContentProps={{ 'aria-describedby': "message-id" }}
-          autoHideDuration={6000}
-          message={<span id="message-id">Incorrect Username/Password</span>}
-        />
-        <Snackbar
-          anchorOrigin={{ vertical: this.state.snackbarVertical, horizontal: this.state.snackbarHorizontal }}
-          open={this.state.snackbarOpen}
-          onClose={this.handleSnackbarClose}
-          autoHideDuration={6000}
-          ContentProps={{ 'aria-describedby': "message-id1" }}
-          message={<span id="message-id1">Logged in successful!</span>}
-        />
+          <Snackbar
+            anchorOrigin={{ vertical: this.state.snackbarVertical, horizontal: this.state.snackbarHorizontal }}
+            open={this.state.snackbarOpen}
+            onClose={this.handleSnackbarClose}
+            autoHideDuration={6000}
+          >
+            <MySnackbarContentWrapper
+              onClose={this.handleSnackbarClose}
+              variant="error"
+              message="Incorrect Username/Password"
+            />
+          </Snackbar>
+          <Snackbar
+            anchorOrigin={{ vertical: this.state.snackbarVertical, horizontal: this.state.snackbarHorizontal }}
+            open={this.state.snackbarOpen}
+            onClose={this.handleSnackbarClose}
+            autoHideDuration={6000}
+          >
+            <MySnackbarContentWrapper
+              onClose={this.handleSnackbarClose}
+              variant="success"
+              message="Logged in successful!"
+            />
+          </Snackbar>
           <CssBaseline>
             <Modal
               handleChange={this.handleChange}
