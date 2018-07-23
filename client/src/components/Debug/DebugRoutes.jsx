63aa5b3c01afc4b23fc7d5050df6b7a8b67da7ca
frogr <austindanielfrench@gmail.com>
2018-07-23 03:49:59 -0700

    added more cohesive styling to the debug tool and a simple form for account settings (nonfunctional)

--- client/src/components/Debug/DebugRoutes.jsx
+++ client/src/components/Debug/DebugRoutes.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+
 import { Link } from 'react-router-dom';
 
 const DebugRoutes = () => {
   return (
-    <div>
-      <h3 className="debug">
-        <Link to="/"> [landing] </Link>
-        <Link to="/trips/"> [trip list] </Link>
-        <Link to="/trips/empty"> [empty trip list] </Link>
-        <Link to="/trips/id/1/"> [trip 1] </Link>
-        <Link to="/trips/create/"> [create trip] </Link>
-        <Link to="/trips/settings/"> [account settings] </Link>
-        <Link to="/trips/billing/"> [billing settings] </Link>
-      </h3>
-    </div>
+    <Paper className="debug">
+      <Link to="/"> [landing] </Link>
+      <Link to="/trips/empty"> [empty trip list] </Link>
+      <Link to="/trips/"> [trip list] </Link>
+      <Link to="/trips/id/1/"> [trip 1] </Link>
+      <Link to="/trips/create/"> [create trip] </Link>
+      <Link to="/trips/settings/"> [account settings] </Link>
+      <Link to="/trips/billing/"> [billing settings] </Link>
+    </Paper>
   );
 };
 
