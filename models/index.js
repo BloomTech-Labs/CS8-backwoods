5893761da796f76678ecc93345c6032468584773
Nick Coultas <nncoultas@yahoo.com>
2018-07-23 15:25:39 -0700

    changed models/index.js to use deployed or local depending on start-up

--- models/index.js
+++ models/index.js
@@ -1,17 +1,21 @@
-// const DATABASE_URL = require('../config')
 const Sequelize = require('sequelize');
 
 //       DB Name     DB Username  DB Password
 // const sequelize = new Sequelize('backwoods', 'postgres', 'dbsuperuser', {
 //   dialect: 'postgres'
 // });
-// for production this will be changed to:
 
-const sequelize = new Sequelize({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-sequelize.connect();
+// for production this will be changed to:
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+  })
+} else {
+  sequelize = new Sequelize('backwoods', 'postgres', 'dbsuperuser', {
+    dialect: 'postgres'
+  })
+}
 
 
 // Test to see if we are connected to sequelize/db
