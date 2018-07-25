e7125810405df0afbe007fdca3c81f62bd2a4dc8
frogr <austindanielfrench@gmail.com>
2018-07-25 11:25:58 -0700

    threw the map in from the other page onto this for styling purposes

--- client/src/components/TripCreate/TripCreate.jsx
+++ client/src/components/TripCreate/TripCreate.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TripCreateForm from './TripCreateForm.jsx';
+import Map from './Map.jsx';
 
 const TripCreate = () => {
   return (
     <div>
       <TripCreateForm />
+      <Map />
     </div>
   );
 };
