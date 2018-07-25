02450b58926a4bb4cc8d4baceea71b8d49b1efe0
frogr <austindanielfrench@gmail.com>
2018-07-25 11:30:07 -0700

    expansion panels added in as well

---
+++ client/src/components/TripCreate/ExpansionPanels.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Coordinates from './Coordinates.jsx';
+import Paper from '@material-ui/core/Paper';
+
+function TripOpen() {
+  return (
+    <Paper className="expansionPanels">
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography>Point 1</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            {Coordinates[0].lat}, {Coordinates[0].lng}
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography>Point 2</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            {Coordinates[1].lat}, {Coordinates[1].lng}
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography>Point 3</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            {Coordinates[2].lat}, {Coordinates[2].lng}
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </Paper>
+  );
+}
+
+export default TripOpen;
--- client/src/components/TripCreate/TripCreate.jsx
+++ client/src/components/TripCreate/TripCreate.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import TripCreateForm from './TripCreateForm.jsx';
 import Map from './Map.jsx';
+import ExpansionPanels from './ExpansionPanels.jsx';
 
 const TripCreate = () => {
   return (
     <div>
       <TripCreateForm />
       <Map />
+      <ExpansionPanels />
     </div>
   );
 };
