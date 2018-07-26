fbe63353b487ee0b7ff9654b13fe281962ef4b6c
frogr <austindanielfrench@gmail.com>
2018-07-26 07:00:40 -0700

    removed markers from map and fixed onclick error

--- client/src/components/TripCreate/Map.jsx
+++ client/src/components/TripCreate/Map.jsx
@@ -35,21 +35,8 @@ export class MapContainer extends React.Component {
             console.log('click');
             this.addMarker(event.LatLng);
             console.log('line 36', event);
-            console.log('line 37', event.latLng.lat());
           }}
-        >
-          <Marker position={Coordinates[0]} />
-          <Marker position={Coordinates[1]} />
-          <Marker position={Coordinates[2]} />
-          <Polygon
-            paths={Coordinates}
-            strokeColor="#0000FF"
-            strokeOpacity={0.8}
-            strokeWeight={2}
-            fillColor="rgba(0, 0, 0, 0)"
-            fillOpacity={0.35}
-          />
-        </Map>
+        />
       </div>
     );
   }
