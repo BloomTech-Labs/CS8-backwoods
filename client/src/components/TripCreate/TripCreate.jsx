bfa0ee49feee9bdb5a3a4bf975b2650c83a75a6b
frogr <austindanielfrench@gmail.com>
2018-07-30 08:59:55 -0700

    aligned the create trip page

--- client/src/components/TripCreate/TripCreate.jsx
+++ client/src/components/TripCreate/TripCreate.jsx
@@ -3,12 +3,14 @@ import TripCreateForm from './TripCreateForm.jsx';
 import Map from './Map.jsx';
 import ExpansionPanels from './ExpansionPanels.jsx';
 
-const TripCreate = (props) => {
+const TripCreate = props => {
   return (
     <div>
       <TripCreateForm email={props.email} getUsersAgain={props.getUsersAgain} />
-      <Map />
-      <ExpansionPanels />
+      <div className="mapAndExpPanels">
+        <Map />
+        <ExpansionPanels />
+      </div>
     </div>
   );
 };
