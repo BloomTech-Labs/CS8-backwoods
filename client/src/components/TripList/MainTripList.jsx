38098685f13f14e987ce31611a8e3bd4aa16bf94
frogr <austindanielfrench@gmail.com>
2018-07-30 17:03:32 -0700

    removed TripList.jsx (unused) and renamed MainTriplist (and all refs) to MainTripList

--- client/src/components/TripList/MainTripList.jsx
+++ client/src/components/TripList/MainTripList.jsx
@@ -3,46 +3,42 @@ import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import Trip from './AddNewTrip'
+import Trip from './AddNewTrip';
 import { Link } from 'react-router-dom';
 
-class MainTriplist extends React.Component {
-
-
+class MainTripList extends React.Component {
   render() {
     return (
       <div>
-        {
-          this.props.trips.map((trip, index) => {
-            return (
-              <div key={index}>
-
-                <Trip tripName={trip.tripName}
-                  createdAt={trip.createdAt}
-                  updatedAt={trip.updatedAt}
-                  startDate={trip.startDate}
-                  endDate={trip.endDate} />
-
-              </div>
-            )
-
-          })}
+        {this.props.trips.map((trip, index) => {
+          return (
+            <div key={index}>
+              <Trip
+                tripName={trip.tripName}
+                createdAt={trip.createdAt}
+                updatedAt={trip.updatedAt}
+                startDate={trip.startDate}
+                endDate={trip.endDate}
+              />
+            </div>
+          );
+        })}
         {console.log(this.props.trips)}
         <Paper className="tripListEmptyPaper" elevation={1}>
-
           <Typography
             className="tripListEmptyPaper-text"
             variant="headline"
             component="h2"
           >
-            Add your first trip!
+            Add a trip!
           </Typography>
           <Link to={`/${this.props.user}/create`}>
             <Button
               variant="fab"
               color="primary"
               aria-label="Add"
-              onClick={this.props.addNewTrip}>
+              onClick={this.props.addNewTrip}
+            >
               <AddIcon />
             </Button>
           </Link>
@@ -50,7 +46,6 @@ class MainTriplist extends React.Component {
       </div>
     );
   }
+}
 
-};
-
-export default MainTriplist;
+export default MainTripList;
--- client/src/components/User/User.jsx
+++ client/src/components/User/User.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Nav from '../Nav/Nav';
-import MainTriplist from '../TripList/MainTripList';
+import MainTripList from '../TripList/MainTripList';
 import TripCreate from '../TripCreate/TripCreate';
 import BillingForm from '../Billing/BillingForm';
 import AccountForm from '../Account/AccountForm';
@@ -18,69 +18,90 @@ class User extends React.Component {
       tripName: '',
       startDate: '',
       endDate: '',
-      hasTrips: false,
-    }
+      hasTrips: false
+    };
   }
 
   componentWillMount() {
     // DEPLOY URL FOR AXIOS CALL
     // axios.get(`https://ancient-inlet-94126.herokuapp.com/${this.props.match.params.user}`).then(res => {
-    
+
     // TEST URL FOR AXIOS CALL
-    axios.get(`http://localhost:8000/${this.props.match.params.user}`).then(res => {
-      if (!res.data) {
-        this.setState({ hasTrips: false });
-        return
-      }
-      this.setState({ hasTrips: true, trips: res.data.trips })
-    }).catch(err => {
-      if (!this.props.isLoggedIn) {
-        this.setState({ noUser: true })
-      }
-      console.log(err);
-    })
+    axios
+      .get(`http://localhost:8000/${this.props.match.params.user}`)
+      .then(res => {
+        if (!res.data) {
+          this.setState({ hasTrips: false });
+          return;
+        }
+        this.setState({ hasTrips: true, trips: res.data.trips });
+      })
+      .catch(err => {
+        if (!this.props.isLoggedIn) {
+          this.setState({ noUser: true });
+        }
+        console.log(err);
+      });
   }
 
   getUsersAgain = () => {
     // DEPLOY URL FOR AXIOS CALL
     // axios.get(`https://ancient-inlet-94126.herokuapp.com/${this.props.match.params.user}`).then(res => {
-    
+
     // TEST URL FOR AXIOS CALL
-    axios.get(`http://localhost:8000/${this.props.match.params.user}`).then(res => {
-      if (!res.data) {
-        this.setState({ hasTrips: false });
-        return
-      }
-      this.setState({ hasTrips: true, trips: res.data.trips })
-    }).catch(err => {
-      if (!this.props.isLoggedIn) {
-        this.setState({ noUser: true })
-      }
-      console.log(err);
-    })
-  }
+    axios
+      .get(`http://localhost:8000/${this.props.match.params.user}`)
+      .then(res => {
+        if (!res.data) {
+          this.setState({ hasTrips: false });
+          return;
+        }
+        this.setState({ hasTrips: true, trips: res.data.trips });
+      })
+      .catch(err => {
+        if (!this.props.isLoggedIn) {
+          this.setState({ noUser: true });
+        }
+        console.log(err);
+      });
+  };
 
   render() {
     return (
       <div>
-        {
-          this.state.noUser ?
-            <Redirect to='/404' />
-            :
-            <div>
-              <Nav user={this.props.email} isLoggedIn={this.props.isLoggedIn} />
-              <Route path="/:user"
-                render={(props) => <MainTriplist {...props}
+        {this.state.noUser ? (
+          <Redirect to="/404" />
+        ) : (
+          <div>
+            <Nav user={this.props.email} isLoggedIn={this.props.isLoggedIn} />
+            <Route
+              path="/:user"
+              render={props => (
+                <MainTripList
+                  {...props}
                   trips={this.state.trips}
                   user={this.props.email}
-                />} exact />
-              <Route path="/:user/create" render={props => (<TripCreate {...props} email={this.props.email} user={this.props.email} getUsersAgain={this.getUsersAgain} />)} />
-              <Route path="/:user/billing" component={BillingForm} />
-              <Route path="/:user/settings" component={AccountForm} />
-            </div>
-        }
+                />
+              )}
+              exact
+            />
+            <Route
+              path="/:user/create"
+              render={props => (
+                <TripCreate
+                  {...props}
+                  email={this.props.email}
+                  user={this.props.email}
+                  getUsersAgain={this.getUsersAgain}
+                />
+              )}
+            />
+            <Route path="/:user/billing" component={BillingForm} />
+            <Route path="/:user/settings" component={AccountForm} />
+          </div>
+        )}
       </div>
-    )
+    );
   }
 }
 
