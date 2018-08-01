317524572ae3dc97bba83225458ecdf0add2d0f6
Aaron Wilder <element1028@gmaill.com>
2018-08-01 16:10:33 -0700

    removed auth from getArchivedTrips and now it's not throwing a 403 error (TODO)Add auth back to this route

--- client/src/components/Archived/Archived.jsx
+++ client/src/components/Archived/Archived.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
-
-const Archived = () => {
-  return (
-    <div>
-      hello
-    </div>
-  )
+import axios from 'axios'
+class Archived extends React.Component {
+  state = {
+    trips: []
+  }
+  componentWillMount() {
+    const token = localStorage.getItem('token');
+    // console.log('match',this.props.match.params.user)
+    // console.log('state from archived',this.state.trips)
+    // console.log(token)
+    axios.get(`http://localhost:8000/${this.props.match.params.user}/getArchivedTrips`,{email: this.props.match.params.user}, { headers: { authorization: token } })
+      .then(res => {
+        this.setState({ trips: res.data.trips })
+        console.log(res)
+    })
+    .catch(error => console.log(error))
+  }
+  render() {
+    return (
+      <div>
+        {/* {console.log(this.props.match.params.user)}
+        {console.log('state from archived',this.state.trips)} */}
+        hello
+      </div>
+    )
+  }
+  
 }
 
 export default Archived;
