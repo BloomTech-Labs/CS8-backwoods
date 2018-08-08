8430db5bc033d09bbfde0014ac89f8435f08fc45
Aaron Wilder <element1028@gmaill.com>
2018-08-08 05:12:16 -0700

    Fix two console errors

--- client/src/components/TripCreate/TripSaveModal.jsx
+++ client/src/components/TripCreate/TripSaveModal.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
 import Paper from '@material-ui/core/Paper';
 import Fade from '@material-ui/core/Fade';
 import Typography from '@material-ui/core/Typography';
