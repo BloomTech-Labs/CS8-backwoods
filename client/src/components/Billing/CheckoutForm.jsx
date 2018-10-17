9a5b679fc03ee5775f61c4b72462ee52f0994fef
wildpow <element1028@gmail.com>
2018-10-17 15:35:11 -0700

    moved checkoutform snackbar into another file and refactored

--- client/src/components/Billing/CheckoutForm.jsx
+++ client/src/components/Billing/CheckoutForm.jsx
@@ -1,34 +1,9 @@
-import React, { Component } from 'react';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-import { CardElement, injectStripe } from 'react-stripe-elements';
-import { Typography } from '../../../node_modules/@material-ui/core';
-import MySnackbarContent from '../Snackbar/MySnackbarContent';
-import Snackbar from '@material-ui/core/Snackbar';
-import green from '@material-ui/core/colors/green';
-import { withStyles } from '@material-ui/core/styles';
-
-const styles1 = theme => ({
-  success: {
-    backgroundColor: green[600]
-  },
-  error: {
-    backgroundColor: theme.palette.error.dark
-  },
-  icon: {
-    fontSize: 20
-  },
-  iconVariant: {
-    opacity: 0.9,
-    marginRight: theme.spacing.unit
-  },
-  message: {
-    display: 'flex',
-    alignItems: 'center'
-  }
-});
-
-const MySnackbarContentWrapper = withStyles(styles1)(MySnackbarContent);
+import React, { Component } from "react";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import { CardElement, injectStripe } from "react-stripe-elements";
+import Typography from "@material-ui/core/Typography";
+import BillingSnackbar from "../Snackbar/BillingSnackbar";
 
 class CheckoutForm extends Component {
   constructor(props) {
@@ -36,88 +11,60 @@ class CheckoutForm extends Component {
     this.state = {
       snackbarPurchase: false,
       snackbarError: false,
-      snackbarVertical: 'top',
-      snackbarHorizontal: 'center',
+      snackbarVertical: "top",
+      snackbarHorizontal: "center"
     };
     this.submit = this.submit.bind(this);
   }
 
-  async submit(ev) {
-    // User clicked submit
-    let { token } = await this.props.stripe.createToken({ name: 'Name' });
-    let response = await fetch('/charge', {
-      method: 'POST',
-      headers: { 'Content-Type': 'text/plain' },
-      body: token.id
-    });
-    if (response.ok) {
-      this.setState({ snackbarPurchase: true })
-    } else {
-      this.setState({ snackbarError: true })
-    }
-  }
-
   handleSnackbarClose = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
     this.setState({ snackbarUnArchive: false });
     this.setState({ snackbarError: false });
   };
 
+  async submit() {
+    // User clicked submit
+    const { stripe } = this.props;
+    const { token } = await stripe.createToken({ name: "Name" });
+    const response = await fetch("/charge", {
+      method: "POST",
+      headers: { "Content-Type": "text/plain" },
+      body: token.id
+    });
+    if (response.ok) {
+      this.setState({ snackbarPurchase: true });
+    } else {
+      this.setState({ snackbarError: true });
+    }
+  }
+
   render() {
+    const { ...snackbarState } = this.state;
     return (
       <Paper className="checkoutForm">
-      <div className="checkoutName">
-      <Typography className="paymentInfo" variant="headline">
-          Payment Information
-        </Typography>
-      </div>
-      <div>
-        <CardElement />
-      </div>
-      <div>
-        <Button
-          id="buyNowButton"
-          onClick={this.submit}
-          variant="contained"
-          color="primary"
-        >
-          Complete Purchase
-        </Button>
-
-      </div>
-           <Snackbar
-          anchorOrigin={{
-            vertical: this.state.snackbarVertical,
-            horizontal: this.state.snackbarHorizontal
-          }}
-          open={this.state.snackbarPurchase}
-          onClose={this.handleSnackbarClose}
-          autoHideDuration={2000}
-        >
-          <MySnackbarContentWrapper
-            onClose={this.handleSnackbarClose}
-            variant="success"
-            message="Purchase Completed Successfully!"
-          />
-        </Snackbar>
-        <Snackbar
-          anchorOrigin={{
-            vertical: this.state.snackbarVertical,
-            horizontal: this.state.snackbarHorizontal
-          }}
-          open={this.state.snackbarError}
-          onClose={this.handleSnackbarClose}
-          autoHideDuration={2000}
-        >
-          <MySnackbarContentWrapper
-            onClose={this.handleSnackbarClose}
-            variant="error"
-            message="Cannot Complete Purchase!"
-          />
-        </Snackbar>
-    </Paper>
+        <BillingSnackbar {...snackbarState} />
+        <div className="checkoutName">
+          <Typography className="paymentInfo" variant="headline">
+            Payment Information
+          </Typography>
+        </div>
+        <div>
+          <CardElement />
+        </div>
+        <div>
+          <Button
+            id="buyNowButton"
+            onClick={this.submit}
+            variant="contained"
+            color="primary"
+          >
+            Complete Purchase
+          </Button>
+        </div>
+      </Paper>
     );
   }
 }
