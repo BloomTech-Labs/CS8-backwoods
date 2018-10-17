409064e882b9d50a0baba14900b81ca28c9bd0a8
wildpow <element1028@gmail.com>
2018-10-16 21:29:28 -0700

    add propTypes to MainSnackbar.jsx

--- client/src/components/Snackbar/MainSnackbar.jsx
+++ client/src/components/Snackbar/MainSnackbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Snackbar from "@material-ui/core/Snackbar";
 import green from "@material-ui/core/colors/green";
@@ -28,115 +29,140 @@ const MySnackbarContentWrapper = withStyles(styles1)(MySnackbarContent);
 // snackbarUserDoesNotExist
 
 const MainSnackbar = props => {
+  const {
+    snackbarVertical,
+    snackbarHorizontal,
+    snackbarLogOut,
+    handleSnackbarClose,
+    snackbarPasswordMismatch,
+    snackbarUserDoesNotExist,
+    snackbarOpenSignIn,
+    snackbarOpenSignUp,
+    snackbarOpenSignUpError,
+    snackbarAuthRedirect
+  } = props;
   return (
     <React.Fragment>
       <Snackbar
         anchorOrigin={{
-          vertical: props.snackbarVertical,
-          horizontal: props.snackbarHorizontal
+          vertical: snackbarVertical,
+          horizontal: snackbarHorizontal
         }}
-        open={props.snackbarLogOut}
-        onClose={props.handleSnackbarClose}
+        open={snackbarLogOut}
+        onClose={handleSnackbarClose}
         autoHideDuration={2000}
       >
         <MySnackbarContentWrapper
-          onClose={props.handleSnackbarClose}
+          onClose={handleSnackbarClose}
           variant="success"
           message="Logged out successfully!"
         />
       </Snackbar>
       <Snackbar
         anchorOrigin={{
-          vertical: props.snackbarVertical,
-          horizontal: props.snackbarHorizontal
+          vertical: snackbarVertical,
+          horizontal: snackbarHorizontal
         }}
-        open={props.snackbarPasswordMismatch}
-        onClose={props.handleSnackbarClose}
+        open={snackbarPasswordMismatch}
+        onClose={handleSnackbarClose}
         autoHideDuration={2000}
       >
         <MySnackbarContentWrapper
-          onClose={props.handleSnackbarClose}
+          onClose={handleSnackbarClose}
           variant="error"
           message="Password does not match"
         />
       </Snackbar>
       <Snackbar
         anchorOrigin={{
-          vertical: props.snackbarVertical,
-          horizontal: props.snackbarHorizontal
+          vertical: snackbarVertical,
+          horizontal: snackbarHorizontal
         }}
-        open={props.snackbarUserDoesNotExist}
-        onClose={props.handleSnackbarClose}
+        open={snackbarUserDoesNotExist}
+        onClose={handleSnackbarClose}
         autoHideDuration={2000}
       >
         <MySnackbarContentWrapper
-          onClose={props.handleSnackbarClose}
+          onClose={handleSnackbarClose}
           variant="error"
           message="User does not exist"
         />
       </Snackbar>
       <Snackbar
         anchorOrigin={{
-          vertical: props.snackbarVertical,
-          horizontal: props.snackbarHorizontal
+          vertical: snackbarVertical,
+          horizontal: snackbarHorizontal
         }}
-        open={props.snackbarOpenSignIn}
-        onClose={props.handleSnackbarClose}
+        open={snackbarOpenSignIn}
+        onClose={handleSnackbarClose}
         autoHideDuration={2000}
       >
         <MySnackbarContentWrapper
-          onClose={props.handleSnackbarClose}
+          onClose={handleSnackbarClose}
           variant="success"
           message="Logged in successful!"
         />
       </Snackbar>
       <Snackbar
         anchorOrigin={{
-          vertical: props.snackbarVertical,
-          horizontal: props.snackbarHorizontal
+          vertical: snackbarVertical,
+          horizontal: snackbarHorizontal
         }}
-        open={props.snackbarOpenSignUp}
-        onClose={props.handleSnackbarClose}
+        open={snackbarOpenSignUp}
+        onClose={handleSnackbarClose}
         autoHideDuration={2000}
       >
         <MySnackbarContentWrapper
-          onClose={props.handleSnackbarClose}
+          onClose={handleSnackbarClose}
           variant="success"
           message="User successfully created!"
         />
       </Snackbar>
       <Snackbar
         anchorOrigin={{
-          vertical: props.snackbarVertical,
-          horizontal: props.snackbarHorizontal
+          vertical: snackbarVertical,
+          horizontal: snackbarHorizontal
         }}
-        open={props.snackbarOpenSignUpError}
-        onClose={props.handleSnackbarClose}
+        open={snackbarOpenSignUpError}
+        onClose={handleSnackbarClose}
         autoHideDuration={2000}
       >
         <MySnackbarContentWrapper
-          onClose={props.handleSnackbarClose}
+          onClose={handleSnackbarClose}
           variant="error"
           message="User already exists!"
         />
       </Snackbar>
       <Snackbar
         anchorOrigin={{
-          vertical: props.snackbarVertical,
-          horizontal: props.snackbarHorizontal
+          vertical: snackbarVertical,
+          horizontal: snackbarHorizontal
         }}
-        open={props.snackbarAuthRedirect}
-        onClose={props.handleSnackbarClose}
+        open={snackbarAuthRedirect}
+        onClose={handleSnackbarClose}
         autoHideDuration={2000}
       >
         <MySnackbarContentWrapper
-          onClose={props.handleSnackbarClose}
+          onClose={handleSnackbarClose}
           variant="error"
           message="Please Sign In"
         />
       </Snackbar>
     </React.Fragment>
   );
+};
+
+MainSnackbar.propTypes = {
+  snackbarOpenSignIn: PropTypes.bool.isRequired,
+  handleSnackbarClose: PropTypes.func.isRequired,
+  snackbarVertical: PropTypes.string.isRequired,
+  snackbarLogOut: PropTypes.bool.isRequired,
+  snackbarHorizontal: PropTypes.string.isRequired,
+  snackbarPasswordMismatch: PropTypes.bool.isRequired,
+  snackbarUserDoesNotExist: PropTypes.bool.isRequired,
+  snackbarOpenSignUp: PropTypes.bool.isRequired,
+  snackbarOpenSignUpError: PropTypes.bool.isRequired,
+  snackbarAuthRedirect: PropTypes.bool.isRequired
 };
 
 export default MainSnackbar;
