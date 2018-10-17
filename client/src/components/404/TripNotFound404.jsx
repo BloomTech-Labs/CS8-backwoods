8eefc05d98e8b4ac35066916c8a10e1433df657a
wildpow <element1028@gmail.com>
2018-10-16 22:01:36 -0700

    refactoring code

--- client/src/components/404/TripNotFound404.jsx
+++ client/src/components/404/TripNotFound404.jsx
@@ -2,21 +2,19 @@ import React from "react";
 import "./404.css";
 import Typography from "@material-ui/core/Typography";
 
-const TripNotFound404 = () => {
-  return (
-    <div className="BadUrl404">
-      <div className="hero-image-404-3">
-        <div className="hero-text">
-          <Typography variant="display4" color="inherit">
-            Unfortunately,
-          </Typography>
-          <Typography variant="display2" color="inherit">
-            This user doesn't have a trip by that name.
-          </Typography>
-        </div>
+const TripNotFound404 = () => (
+  <div className="BadUrl404">
+    <div className="hero-image-404-3">
+      <div className="hero-text">
+        <Typography variant="display4" color="inherit">
+          Unfortunately,
+        </Typography>
+        <Typography variant="display2" color="inherit">
+          This user doesn&apos;t have a trip by that name.
+        </Typography>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default TripNotFound404;
--- client/src/components/404/UserHasNoTrips404.jsx
+++ client/src/components/404/UserHasNoTrips404.jsx
@@ -2,21 +2,19 @@ import React from "react";
 import "./404.css";
 import Typography from "@material-ui/core/Typography";
 
-const UserHasNoTrips404 = () => {
-  return (
-    <div className="BadUrl404">
-      <div className="hero-image-404-4">
-        <div className="hero-text">
-          <Typography variant="display4" color="inherit">
-            Apologies.
-          </Typography>
-          <Typography variant="display2" color="inherit">
-            This wanderer as not informed us of any trips yet...
-          </Typography>
-        </div>
+const UserHasNoTrips404 = () => (
+  <div className="BadUrl404">
+    <div className="hero-image-404-4">
+      <div className="hero-text">
+        <Typography variant="display4" color="inherit">
+          Apologies.
+        </Typography>
+        <Typography variant="display2" color="inherit">
+          This wanderer as not informed us of any trips yet...
+        </Typography>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default UserHasNoTrips404;
