054368a2d6bb24428d1aaabebbdf94f5b0fa625a
wildpow <element1028@gmail.com>
2018-10-17 14:19:08 -0700

    snackbar for account setting is working

--- client/src/components/Account/AccountForm.jsx
+++ client/src/components/Account/AccountForm.jsx
@@ -7,17 +7,15 @@ import {
   createMuiTheme
 } from "@material-ui/core/styles";
 import Fade from "@material-ui/core/Fade";
-
 import TextField from "@material-ui/core/TextField";
 import Icon from "@material-ui/core/Icon";
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
-import Snackbar from "@material-ui/core/Snackbar";
 import green from "@material-ui/core/colors/green";
-import "./Account.css";
 import Typography from "@material-ui/core/Typography";
+import AccountSnackBar from "../Snackbar/AccountSnackBar";
+import "./Account.css";
 import API_URL from "../../API_URL";
-import MySnackbarContent from "../Snackbar/MySnackbarContent";
 
 const theme = createMuiTheme({
   palette: {
@@ -31,7 +29,7 @@ const theme = createMuiTheme({
     }
   }
 });
-const styles = theme => ({
+const styles = () => ({
   button: {
     margin: "auto",
     textAlign: "center"
@@ -42,28 +40,6 @@ const styles = theme => ({
   //   width: 200
   // }
 });
-
-const styles1 = theme => ({
-  success: {
-    backgroundColor: green[600]
-  },
-  error: {
-    backgroundColor: theme.palette.error.dark
-  },
-  icon: {
-    fontSize: 20
-  },
-  iconVariant: {
-    opacity: 0.9,
-    marginRight: theme.spacing.unit
-  },
-  message: {
-    display: "flex",
-    alignItems: "center"
-  }
-});
-
-const MySnackbarContentWrapper = withStyles(styles1)(MySnackbarContent);
 
 class AccountForm extends React.Component {
   state = {
@@ -83,7 +59,7 @@ class AccountForm extends React.Component {
   };
 
   handleSubmit = e => {
-    console.log("in submit");
+    // console.log("in submit");
     e.preventDefault();
     const token = localStorage.getItem("token");
     const { email, password } = this.state;
@@ -113,98 +89,68 @@ class AccountForm extends React.Component {
 
   render() {
     const { classes } = this.props;
-
+    const { password, oldPassword, email, ...snackbarState } = this.state;
     return (
-      <Fade in={true}>
-        <div className="accountWrapper">
-          {console.log(this.state)}
-          <Paper className="formPaper">
-            <Typography variant="headline">Change Password</Typography>
-            <form onSubmit={this.handleSubmit} className="accountForm">
-              <div className="accountTextFieldWrapper">
-                <MuiThemeProvider theme={theme}>
-                  <TextField
-                    required
-                    id="required"
-                    label="Email"
-                    type="email"
-                    value={this.state.email}
-                    onChange={this.handleChange("email")}
-                    autoComplete="email"
-                    // className={classes.textField}
-                    margin="normal"
-                  />
-                  <TextField
-                    required
-                    // id="password-input"
-                    label="Old Password"
-                    value={this.state.oldPassword}
-                    onChange={this.handleChange("oldPassword")}
-                    // className={classes.textField}
-                    type="password"
-                    autoComplete="current-password"
-                    margin="normal"
-                  />
-                  <TextField
-                    required
-                    // id="password-input"
-                    label="New Password"
-                    value={this.state.password}
-                    onChange={this.handleChange("password")}
-                    // className={classes.textField}
-                    type="password"
-                    margin="normal"
-                  />
-                </MuiThemeProvider>
-              </div>
+      <React.Fragment>
+        <AccountSnackBar
+          handleSnackbarClose={this.handleSnackbarClose}
+          {...snackbarState}
+        />
+        <Fade in>
+          <div className="accountWrapper">
+            <Paper className="formPaper">
+              <Typography variant="headline">Change Password</Typography>
+              <form onSubmit={this.handleSubmit} className="accountForm">
+                <div className="accountTextFieldWrapper">
+                  <MuiThemeProvider theme={theme}>
+                    <TextField
+                      required
+                      id="required"
+                      label="Email"
+                      type="email"
+                      value={email}
+                      onChange={this.handleChange("email")}
+                      autoComplete="email"
+                      margin="normal"
+                    />
+                    <TextField
+                      required
+                      label="Old Password"
+                      value={oldPassword}
+                      onChange={this.handleChange("oldPassword")}
+                      type="password"
+                      autoComplete="current-password"
+                      margin="normal"
+                    />
+                    <TextField
+                      required
+                      label="New Password"
+                      value={password}
+                      onChange={this.handleChange("password")}
+                      type="password"
+                      margin="normal"
+                    />
+                  </MuiThemeProvider>
+                </div>
 
-              <div className="accountSubmitWrapper">
-                <MuiThemeProvider theme={theme}>
-                  <Button
-                    variant="contained"
-                    className={classes.button}
-                    type="submit"
-                    color="primary"
-                  >
-                    Submit
-                    <Icon className="accountSubmitIcon">send</Icon>
-                  </Button>
-                </MuiThemeProvider>
-              </div>
-            </form>
-          </Paper>
-          <Snackbar
-            anchorOrigin={{
-              vertical: this.state.snackbarVertical,
-              horizontal: this.state.snackbarHorizontal
-            }}
-            open={this.state.snackbarChange}
-            onClose={this.handleSnackbarClose}
-            autoHideDuration={2000}
-          >
-            <MySnackbarContentWrapper
-              onClose={this.handleSnackbarClose}
-              variant="success"
-              message="Changed Password Successfully!"
-            />
-          </Snackbar>
-          <Snackbar
-            anchorOrigin={{
-              vertical: this.state.snackbarVertical,
-              horizontal: this.state.snackbarHorizontal
-            }}
-            open={this.state.snackbarError}
-            onClose={this.handleSnackbarClose}
-            autoHideDuration={2000}
-          >
-            <MySnackbarContentWrapper
-              onClose={this.handleSnackbarClose}
-              variant="error"
-              message="Must Be Logged In To Change Password!"
-            />
-          </Snackbar>
-        </div>
-      </Fade>
+                <div className="accountSubmitWrapper">
+                  <MuiThemeProvider theme={theme}>
+                    <Button
+                      variant="contained"
+                      className={classes.button}
+                      type="submit"
+                      color="primary"
+                    >
+                      Submit
+                      <Icon className="accountSubmitIcon">send</Icon>
+                    </Button>
+                  </MuiThemeProvider>
+                </div>
+              </form>
+            </Paper>
+          </div>
+        </Fade>
+      </React.Fragment>
     );
   }
 }
--- client/src/components/Snackbar/AccountSnackBar.jsx
+++ client/src/components/Snackbar/AccountSnackBar.jsx
@@ -4,3 +4,79 @@ import { withStyles } from "@material-ui/core/styles";
 import Snackbar from "@material-ui/core/Snackbar";
 import green from "@material-ui/core/colors/green";
 import MySnackbarContent from "./MySnackbarContent";
+
+const styles1 = theme => ({
+  success: {
+    backgroundColor: green[600]
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark
+  },
+  icon: {
+    fontSize: 20
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing.unit
+  },
+  message: {
+    display: "flex",
+    alignItems: "center"
+  }
+});
+
+const MySnackbarContentWrapper = withStyles(styles1)(MySnackbarContent);
+
+const AccountSnackBar = props => {
+  const {
+    snackbarVertical,
+    snackbarHorizontal,
+    snackbarChange,
+    handleSnackbarClose,
+    snackbarError
+  } = props;
+  return (
+    <React.Fragment>
+      <Snackbar
+        anchorOrigin={{
+          vertical: snackbarVertical,
+          horizontal: snackbarHorizontal
+        }}
+        open={snackbarChange}
+        onClose={handleSnackbarClose}
+        autoHideDuration={2000}
+      >
+        <MySnackbarContentWrapper
+          onClose={handleSnackbarClose}
+          variant="success"
+          message="Changed Password Successfully!"
+        />
+      </Snackbar>
+      <Snackbar
+        anchorOrigin={{
+          vertical: snackbarVertical,
+          horizontal: snackbarHorizontal
+        }}
+        open={snackbarError}
+        onClose={handleSnackbarClose}
+        autoHideDuration={2000}
+      >
+        <MySnackbarContentWrapper
+          onClose={handleSnackbarClose}
+          variant="error"
+          message="Must Be Logged In To Change Password!"
+        />
+      </Snackbar>
+    </React.Fragment>
+  );
+};
+
+AccountSnackBar.propTypes = {
+  snackbarVertical: PropTypes.string.isRequired,
+  snackbarHorizontal: PropTypes.string.isRequired,
+  snackbarChange: PropTypes.bool.isRequired,
+  snackbarError: PropTypes.bool.isRequired,
+  handleSnackbarClose: PropTypes.func.isRequired
+};
+
+export default AccountSnackBar;
